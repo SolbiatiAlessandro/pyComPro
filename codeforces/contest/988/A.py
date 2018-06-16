2b8fd555de7c28c82a14513b7760cf39b6ad0d44
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-16 11:17:21 +0900

    988/A AC (revised solution)

--- codeforces/contest/988/A.py
+++ codeforces/contest/988/A.py
@@ -1,30 +1,7 @@
-from sys import stdin, stdout
-
-
-def main():
-    n, k = [int(x) for x in stdin.readline().split()]
-    a = [int(x) for x in stdin.readline().split()]
-
-    res = []
-    support = []
-
-    for i in range(len(a)):
-        x = a[i]
-        new = 1
-        for y in support:
-            if x == y:
-                new = 0
-        if new:
-            support.append(x)
-            res.append(i)
-
-    if len(res) < k:
-        stdout.write("NO")
-    else:
-        stdout.write("YES\n")
-        for i in range(0, k):
-            stdout.write(str(res[i]+1)+" ")
-
-
-if __name__ == "__main__":
-    main()
+n, k = map(int, input().split())
+s, v = set(), []
+for i, ai in enumerate(map(int, input().split())):
+    if ai not in s:
+        s.add(ai)
+        v += [str(i+1)]
+print('YES\n'+' '.join(v[:k]) if len(v) >= k else 'NO')
