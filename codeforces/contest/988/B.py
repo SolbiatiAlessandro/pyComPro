0cbbf39924192541fbc84be99d572609729531a6
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-16 22:27:54 +0900

    988/B AC 76ms (revised)

--- codeforces/contest/988/B.py
+++ codeforces/contest/988/B.py
@@ -1,20 +1,7 @@
 n = int(input())
-a = []
-for i in range(n):
-    a += [input()]
+a = sorted((input() for _ in range(n)), key=lambda x: len(x))
+v = all(a[i] in a[i+1] for i in range(n-1))
+print('YES\n'+"\n".join(a) if v else 'NO')
 
 
-def solve(a, n):
-    for i in range(n):
-        for j in range(n-1):
-            if a[j+1] in a[j]:
-                a[j+1], a[j] = a[j], a[j+1]
-            elif len(a[j+1]) < len(a[j]):
-                return print("NO")
-    print("YES")
-    for aa in a:
-        print(aa)
-
-
-solve(a, n)
 
