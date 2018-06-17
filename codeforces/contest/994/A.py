9abb1ddce7b85fa0ed61233c297233bacf665a80
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-18 02:49:22 +0900

    994/A AC (revised)

--- codeforces/contest/994/A.py
+++ codeforces/contest/994/A.py
@@ -1,7 +1,5 @@
-n, m = map(int, input().split())
-a, b, res = map(int, input().split()), set(map(int, input().split())), []
-for x in a:
-    if x in b:
-        res += [x]
-print(*res)
+R = lambda: (int, input().split())
+n, a, b = R(), R(), R()
+print(' '.join(map(str, (x for x in a if x in b))))
+
 
