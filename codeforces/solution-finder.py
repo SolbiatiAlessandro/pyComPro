2f9431c65824a93a2b4f4a89e80ff6529350a581
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-18 01:33:17 +0900

    div1 div2 feature

--- codeforces/solution-finder.py
+++ codeforces/solution-finder.py
@@ -3,10 +3,10 @@ import argparse
 import time
 from bs4 import BeautifulSoup
 
-accepted_color = set(["user-orange","user-violet","user-legendary","user-red"])
+accepted_color_div1 = set(["user-orange","user-violet","user-legendary","user-red"])
+accepted_color_div2 = set(["user-blue", "user-cyan"])
 
-
-def parsepage(html, _problem):
+def parsepage(html, _problem, div):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find("table", {"class": "status-frame-datatable"})
     res = []
@@ -14,7 +14,12 @@ def parsepage(html, _problem):
         try:
             language = table.contents[row].contents[9].text[10:-6]
             if language == "Python 3" or language == "Python 2":
-                color = table.contents[row].contents[5].contents[1].get('class')[1] in accepted_color
+                if div == 1:
+                    color = table.contents[row].contents[5].contents[1].get('class')[1] in accepted_color_div1
+                elif div == 2:
+                    color = table.contents[row].contents[5].contents[1].get('class')[1] in accepted_color_div2
+                else:
+                    color = 1
                 problem = table.contents[row].contents[7].text[19:20] == _problem
                 verdict = table.contents[row].contents[11].text[1:-1] == "Accepted"
                 if(color and problem and verdict):
@@ -35,6 +40,7 @@ def main():
                         help=':number of the contest e.g. 988')
     parser.add_argument('problem_number', type=str,
                         help=':number of the problem e.g. A')
+    parser.add_argument('div', type=int, help=':1 (div1) or 2 (div2) contest')
     """
     parser.
     add_argument('--sum', dest='accumulate', action='store_const',
@@ -42,13 +48,14 @@ def main():
                        help='sum the integers (default: find the max)')
     """
     args = parser.parse_args()
+
     f = open("./contest/"+str(args.contest_number) + "/"+str(args.problem_number)+".html", "w")
     init = "<style>.cflink{padding: 8px;box-shadow: 0px 0px 8px grey;background: aliceblue;line-height: 40px;}</style><p>This is a list of all the python AC submission from high-ranking users to <b>contest "+str(args.contest_number)+"</b> for the <b>problem "+str(args.problem_number)+"</p>"
 
     f.write(init)
 
     html = urllib2.urlopen("http://codeforces.com/contest/"+str(args.contest_number)+"/status/page/1?order=BY_JUDGED_DESC").read()
-    _urls = parsepage(html,args.problem_number)
+    _urls = parsepage(html,args.problem_number,args.div)
     writeurls(f,_urls)
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -59,7 +66,7 @@ def main():
         print("request : "+str(page)+"/"+pages+" | approx. time left : "+str(((int(pages)-page)*0.5)/60)+" mins")
         html = urllib2.urlopen(
             "http://codeforces.com/contest/" + str(args.contest_number) + "/status/page/"+str(page)+"?order=BY_JUDGED_DESC").read()
-        _urls = parsepage(html,args.problem_number)
+        _urls = parsepage(html,args.problem_number,args.div)
         writeurls(f, _urls)
         time.sleep(0.5)
 
