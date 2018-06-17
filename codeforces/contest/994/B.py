a7ca82ba753d79db101bf62bf24a90f4c5937098
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-18 02:32:24 +0900

    994/B AC (revised)

--- codeforces/contest/994/B.py
+++ codeforces/contest/994/B.py
@@ -1,16 +1,11 @@
-n, k = map(int, input().split())
-power, coins, kings, res = list(map(int, input().split())), list(map(int,input().split())), [], []
-for i in range(n):
-    kings += [(power[i], coins[i])]
-s = sorted(kings, key=lambda x: x[1], reverse=1)
-for king in kings:
-    cnt, base = 0, 0
-    cum = 0
-    while cnt < k and base < len(s):
-        if s[base][0] < king[0]:
-            cum += s[base][1]
-            cnt += 1
-        base += 1
-    res += [cum+king[1]]
-print(*res)
-
+R = lambda: map(int, input().split())
+n, k = R()
+v = []
+t = [0]*n
+for p, c, i in sorted(zip(R(), R(), range(n))):
+    t[i] = sum(v)+c
+    v += [c]
+    v = sorted(v)[::-1]
+    if len(v) > k:
+        v.pop()
+print(' '.join(map(str, t)))
