b5c381ae80184ab8fee261cffdb6427835d0e59b
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-06-25 06:54:45 +0900

    996/B AC (revised)

--- codeforces/contest/996/B.py
+++ codeforces/contest/996/B.py
@@ -1,11 +1,5 @@
 import math
-R = lambda : list(map(int, input().split()))
-n, a, m, res = int(input()), R(), 10e9,-1
-k = ((i,math.ceil((ai-i)/n)) for i,ai in enumerate(a))
-for _ in range(n):
-	t = next(k)
-	if(t[1]<m):
-		m=t[1]
-		res = t[0]
-print(res+1)
+n, a, m, res = int(input()), [*(map(int,input().split()))], 10e9,-1
+k = [ math.ceil((ai-i)/n) for i,ai in enumerate(a) ]
+print(k.index(min(k))+1)
 	
