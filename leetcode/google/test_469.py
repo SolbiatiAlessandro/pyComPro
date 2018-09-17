88aa7f9f5704c655fc8ea600542a0b600c137251
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-09-17 15:16:10 -0700

    l469.py failing tests, need to be rewritten with better algorithm

--- leetcode/google/test_469.py
+++ leetcode/google/test_469.py
@@ -50,10 +50,31 @@ class testSolution( unittest.TestCase ):
         self.assertEqual( res, -1 )
 
 
+    def test_repeatMoreThanTwoTimes( self ):
 
+        A, B = "kuku", "kukukukukukukuku"
+        res = self.s.repeatedStringMatch( A, B ) 
+        #import pdb;pdb.set_trace()
+        self.assertEqual( res, 4 )
 
+        A, B = "kuku", "kukukukukukukukuk"
+        res = self.s.repeatedStringMatch( A, B ) 
+        #import pdb;pdb.set_trace()
+        self.assertEqual( res, 5 )
 
+    def test_alreadySubstring( self ):
 
+        A, B = "kuku", "kuk"
+        res = self.s.repeatedStringMatch( A, B ) 
+        #import pdb;pdb.set_trace()
+        self.assertEqual( res, 1 )
+
+    def test_tailTouchFront( self ):
+
+        A, B = "kukk", "kkkukk"
+        res = self.s.repeatedStringMatch( A, B ) 
+        #import pdb;pdb.set_trace()
+        self.assertEqual( res, 2 )
 
 if __name__ == "__main__":
     unittest.main()
