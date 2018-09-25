858d58bd53d628444080b71beb702bca98443d70
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-09-25 09:44:05 +0800

    336 AC : added more detailed comments

--- leetcode/google/346/l336.py
+++ leetcode/google/346/l336.py
@@ -2,6 +2,8 @@ def update( cnt, cell ):
     """
     return the updated value of cell based on cnt
     :rtype int in [-1,0,1,2]
+
+    update is the logic to compute the dummy value
     """
     if cell == 1:
         if cnt < 2 or cnt > 3:
@@ -16,6 +18,9 @@ def cnt( board, x, y ):
     """
     :board, input board
     :x,y (int) where is the cell to execute cnt on
+
+    the cnt function iterate over the (max) 8 cells surrounding x,y
+    and overwrite the contents with the new dummy value
     """
     res = 0
     for i in xrange(-1+x,2+x):
@@ -44,13 +49,25 @@ class Solution(object):
         """
         :type board: List[List[int]]
         :rtype: void Do not return anything, modify board in-place instead.
+
+        the solution is in-place an achieve O(1) memory using a dummy value trick:
+        i am updating the next state of a cell using the following table
+
+          value | current state | next state |
+            0           0             0
+            1           1             1
+            2           0             1
+            -1          1             0
+
+        with this trick you can overwrite in place without the need of additional memory,
+        and when you look back at the cell later you know what was the previous state
         """
         #import pdb;pdb.set_trace()
         for x in xrange(len(board[0])):
             for y in xrange(len(board)):
-                newCount = cnt( board, x, y)
-                board[y][x] = update( newCount, board[y][x] )
-        cleanBoard( board )
+                newCount = cnt( board, x, y) # this is counting the live cells surrounding x,y
+                board[y][x] = update( newCount, board[y][x] ) # this overwrites the board in place
+        cleanBoard( board ) # this clear the board from dummy values
        
         
         
