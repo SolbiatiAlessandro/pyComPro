33e31d89646493a3bf2f4e6c081e6453aa54ca93
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-09-25 17:29:30 +0800

    396 (API no test)

---
+++ leetcode/google/396/l396.py
@@ -0,0 +1,19 @@
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        res, helper = min( n, read4( buf ) ), []
+        while res == n and len(buf) < n:
+            res = read4( helper )
+            buf += helper
+        del( buf[n:] )
+        return len(buf)
+        
---
+++ leetcode/google/396/test_396.py
@@ -0,0 +1,14 @@
+import unittest
+import l396
+
+class testSolution( unittest.TestCase ):
+
+    def setUp( self ):
+        self.s = l396.Solution()
+
+        """
+        can not test because of API in solution
+        """
+
+if __name__ == "__main__":
+    unittest.main()
