71469d7e81a6f15b48389ee596cd97ccadaa3125
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-13 21:32:02 +0800

    366 wrote solution draft

---
+++ leetcode/google/366/l366.py
@@ -0,0 +1,72 @@
+"https://leetcode.com/explore/interview/card/google/61/trees-and-graphs/366/"
+
+
+COLOR = {}
+CHILDREN = {}
+PARENT = {}
+
+
+def dfs(vertex, highest_index):
+    """dfs to check for cycles
+    Args:
+        vertex: to be visited
+        highest_index: highest_index of the edges visited so far
+    Returns:
+        highest_index if detects cycles, None otherwise
+    """
+    if vertex not in COLOR:
+        COLOR[vertex] = 0
+        if CHILDREN.get(vertex):
+            for children, index in CHILDREN[vertex]:
+                got = dfs(children, max(highest_index, index))
+                if got:
+                    return got
+        COLOR[vertex] = 1
+    elif COLOR[vertex] == 0:
+        if highest_index == -1:
+            exit('error: highest_index -1')
+        return highest_index
+    return None
+
+
+def last_edge_in_cycle():
+    """launch dfs on every vertex
+    Returns:
+        None: if there is no cycles
+        edge: edge in the cycle comparing last in edges list
+    """
+    for vertex, _ in CHILDREN.items():
+        if vertex not in COLOR:
+            got = dfs(vertex, -1)
+            if got:
+                return got
+    return None
+
+
+class Solution(object):
+    def findRedundantDirectedConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        second, first = None, None  # candidate edges to remove
+        for index, edge in enumerate(edges):
+            if edge[1] not in PARENT:  # add edge to PARENT dict
+                PARENT[edge[1]] = edge[0]
+            else:
+                second = edge
+                first = [PARENT[edge[1]], edge[1]]
+            if edge[0] not in CHILDREN:  # add edge to CHILDREN dict
+                CHILDREN[edge[0]] = [[edge[1], index]]
+            else:
+                CHILDREN[edge[0]].append([edge[1], index])
+
+        if first:  # deleting second candidate and check cycles
+            for index, child in enumerate(CHILDREN[second[0]]):
+                if child[0] == first[1]:
+                    del CHILDREN[second[0]][index]
+            if last_edge_in_cycle():  # if there is cycle
+                return first
+            return second
+        else:
+            return edges[last_edge_in_cycle()]  # there must be one cycle
---
+++ leetcode/google/366/test_366.py
@@ -0,0 +1,75 @@
+import unittest
+import l366
+
+def clean():
+    """clean globals"""
+    l366.CHILDREN = {}
+    l366.COLOR = {}
+    l366.PARENT = {}
+
+class testSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.s = l366.Solution()
+    
+    def test_last_edge(self):
+        l366.CHILDREN = {1: [[4, 3]], 2: [[1, 0]], 3: [[1, 1]], 4: [[2, 2], [5,4]]}
+        got = l366.last_edge_in_cycle()
+        self.assertEqual(got, 3)
+
+    def test_last_edge_base_example(self):
+        l366.CHILDREN = {1: [[2, 0], [5, 4]], 2: [[3, 1]], 3: [[4, 2]], 4: [[1, 3]]}
+        l366.COLOR = {}
+        got = l366.last_edge_in_cycle()
+        self.assertEqual(got, 3)
+        clean()
+
+        #import pdb;pdb.set_trace()
+        l366.CHILDREN = {1: [[5, 3], [2, 4]], 2: [[3, 0]], 3: [[4, 1]], 4: [[1, 2]]}
+        l366.COLOR = {}
+        got = l366.last_edge_in_cycle()
+        self.assertEqual(got, 4)
+
+        print "ok2"
+        
+
+    #@unittest.skip("wait")
+    def test_solution(self):
+        #import pdb;pdb.set_trace()
+        test_input = [[1, 2], [1, 3], [2, 3]]
+        expected = [2, 3]
+        got = self.s.findRedundantDirectedConnection(test_input)
+        self.assertEqual(got, expected)
+        clean()
+
+        #import pdb;pdb.set_trace()
+        test_input = [[1, 2], [2, 3], [3, 4], [4, 1], [1, 5]]
+        expected = [4, 1]
+        got = self.s.findRedundantDirectedConnection(test_input)
+        self.assertEqual(got, expected)
+        clean()
+
+        #import pdb;pdb.set_trace()
+        test_input = [[2, 3], [3, 4], [4, 1], [1, 5], [1, 2]]
+        expected = [1, 2]
+        got = self.s.findRedundantDirectedConnection(test_input)
+        self.assertEqual(got, expected)
+        clean()
+
+        test_input = [[1, 2], [2, 3], [3, 4], [3, 3], [1, 5]]
+        expected = [3, 3]
+        got = self.s.findRedundantDirectedConnection(test_input)
+        self.assertEqual(got, expected)
+        clean()
+
+        test_input = [[2, 1], [3, 1], [4, 2], [4, 5], [1, 4]]
+        expected = [2, 1]
+        got = self.s.findRedundantDirectedConnection(test_input)
+        self.assertEqual(got, expected)
+        clean()
+
+        print "okk"
+
+
+if __name__ == "__main__":
+    unittest.main()
