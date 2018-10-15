3bf7b910bc071489aeace2864974960305d10c1d
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-15 16:31:48 +0800

    369 AC beats 95% (optimized dfs)

--- leetcode/google/369/l369.py
+++ leetcode/google/369/l369.py
@@ -1,20 +1,3 @@
-colors, children = [], []
-
-def init(numCourses):
-    global colors
-    global children
-    colors, children = [None]*numCourses, [[]]*numCourses 
-
-def dfs(vertex):
-    colors[vertex] = 0
-    for child in children[vertex]:
-        if colors[child] == 0:
-            return False
-        if not dfs(child):
-            return False
-    colors[vertex] = 1
-    return True
-
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -22,14 +5,25 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        init(numCourses)
-        for edge in prerequisites:  # build adj list
-            if not children[edge[0]]:
-                children[edge[0]] = [edge[1]]
-            else:
-                children[edge[0]].append(edge[1])
-        for vertex in xrange(numCourses):
-            if colors[vertex] is None:
-                if not dfs(vertex):
+        colors = [None for _ in xrange(numCourses)]
+        children = [[] for _ in xrange(numCourses)]
+        for first, second in prerequisites:  # build adj list
+            children[first].append(second)
+
+        def dfs(vertex):
+            if colors[vertex] == 0:
+                return False
+            if colors[vertex] == 1:
+                return True
+            colors[vertex] = 0
+            for child in children[vertex]:
+                if not dfs(child):
                     return False
+            colors[vertex] = 1
+            return True
+
+        for vertex in xrange(numCourses):
+            if not dfs(vertex):
+                return False
+
         return True
