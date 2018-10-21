8cb9c55dce868d02688aeb163cbac7a9d4d0b157
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-21 16:48:48 +0800

    wrote new solution with prefix trie, on profiling run fast as binary search solution

---
+++ leetcode/google/370/l370d.py
@@ -0,0 +1,111 @@
+from collections import defaultdict
+
+
+class Node(object):
+    def __init__(self, letter):
+        self.char = letter
+        self.children = defaultdict(list)
+
+
+def build_prefix_trie(words):
+    """given a list of words build a prefix tree
+
+    Args:
+        words: list[str]
+
+    Return:
+        tree: instance of Node(), root of the (trie) tree
+    """
+    LEN_WORD = len(words[0])
+    tree = Node('*')
+    for word_index, word in enumerate(words):
+        letter_index, curr = 0, tree
+
+        while curr.children[word[letter_index]]:  # already existing
+            curr = curr.children[word[letter_index]]
+            letter_index += 1
+
+        while letter_index < LEN_WORD:  # create new nodes
+            new_node = Node(word[letter_index])
+            curr.children[word[letter_index]] = new_node
+            curr = new_node
+            letter_index += 1
+        curr.index = word_index
+    return tree
+
+
+def get_matches(trie, query):
+    """
+    get children from a query on the trie
+
+    Traverse the prefix trie following the letters on query,
+    once query is finished it runs a dfs to traverse all paths
+    and get all children from there.
+
+    Args:
+        trie: instance of node
+        query: string "ba" (from "ball")
+
+    Returns:
+        list[int] : index of matches
+    """
+    for letter in query:
+        if trie:
+            trie = trie.children[letter]
+        if not trie:
+            return []
+    if hasattr(trie, 'index'):
+        return [trie.index]
+    res, stack = [], [trie]  # run dfs
+    while stack:
+        node = stack.pop()
+        if hasattr(node, 'index'):
+            res.append(node.index)
+        else:
+            for _, child in node.children.items():
+                if child:
+                    stack.append(child)
+    return res
+
+
+class Solution(object):
+    def wordSquares(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[str]]
+        """
+        WORD_LEN = len(words[0])
+        res = []
+        trie = build_prefix_trie(words)
+
+        def solve(solution):
+            """recursive call for the solution of the task
+
+            Given a valid subsolution, iterate over all matches and call itself
+            on other subsolution with length = len(solution) + 1,
+            if is the desired length it appends the results to res
+
+            Args:
+                solution: list[int], list of indexes of words of sub-solutions
+            """
+            if len(solution) == WORD_LEN:
+                return res.append(solution[:])
+            query, index = "", len(solution)
+            for i in xrange(len(solution)):
+                query += words[solution[i]][index]
+
+            matches = get_matches(trie, query)
+            for match in matches:
+                if type(match) is not type(1):
+                    import pdb;pdb.set_trace()
+                    matches = get_matches(trie, query)
+                    exit("type error")
+                solve(solution + [match])
+
+        for i in xrange(len(words)):
+            solve([i])
+
+        for solution_index, solution in enumerate(res):
+            for word_index, word in enumerate(solution):
+                res[solution_index][word_index] = words[word]
+        return res
---
+++ leetcode/google/370/test_370d.py
@@ -0,0 +1,92 @@
+import unittest
+import l370d
+
+
+class testSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.s = l370d.Solution()
+        self.words = ["area","lead","wall","lady","ball"]
+        self.s.words = ["area","lead","wall","lady","ball"]
+
+    def test_trie(self):
+        bt = l370d.build_prefix_trie
+        trie = bt(self.words)
+        self.assertEqual(trie.char, '*')
+        self.assertFalse(trie.children["d"])
+        self.assertTrue(trie.children["l"])
+        self.assertEqual(trie.children["a"].children["r"].children["e"]\
+                .children["a"].index, 0)
+        self.assertEqual(trie.children["b"].children["a"].children["l"]\
+                .children["l"].index, 4)
+        self.assertEqual(trie.children["l"].children["a"].children["d"]\
+                .children["y"].index, 3)
+        self.assertEqual(trie.children["w"].children["a"].children["l"]\
+                .children["l"].index, 2)
+        self.assertEqual(trie.children["l"].children["e"].children["a"]\
+                .children["d"].index, 1)
+
+    def test_get_matches(self):
+        gm = l370d.get_matches
+        words = l370d.build_prefix_trie(self.s.words)
+        got = gm(words, "b")
+        self.assertItemsEqual(got, [4])
+        got = gm(words, "l")
+        self.assertItemsEqual(got, [1,3])
+        got = gm(words, "r")
+        self.assertFalse(got)
+        got = gm(words, "lk")
+        self.assertFalse(got)
+        got = gm(words, "rk")
+        self.assertFalse(got)
+
+        print"ookk"
+
+        raw = ["abbba", "abbbc", "abbbd", "abbaa", "abbka", "abbia", "abjaa"]
+        words = l370d.build_prefix_trie(raw)
+        got = gm(words, "a")
+        self.assertItemsEqual(got, range(len(raw)))
+        got = gm(words, "ab")
+        self.assertItemsEqual(got, range(len(raw)))
+        got = gm(words, "abb")
+        self.assertItemsEqual(got, range(len(raw)-1))
+        got = gm(words, "abbb")
+        self.assertItemsEqual(got, range(3))
+
+    
+    #@unittest.skip("wait")
+    def test_solution(self):
+        words = ["area","lead","wall","lady","ball"]
+        expected = [
+          [ "wall",
+            "area",
+            "lead",
+            "lady"
+          ],
+          [ "ball",
+            "area",
+            "lead",
+            "lady"
+          ]
+        ]
+        got = self.s.wordSquares(words)
+        self.assertItemsEqual(got, expected)
+        words = ["abat","baba","atan","atal"]
+        expected = [
+          [ "baba",
+            "abat",
+            "baba",
+            "atan"
+          ],
+          [ "baba",
+            "abat",
+            "baba",
+            "atal"
+          ]
+        ]
+        got = self.s.wordSquares(words)
+        self.assertItemsEqual(got, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
