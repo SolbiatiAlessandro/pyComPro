17413c2bd65e4ba5e27dd2a7bbdf9d82ce3a938f
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-10-22 13:32:18 +0800

    399 AC, beats 70%

--- leetcode/google/399/l399.py
+++ leetcode/google/399/l399.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        def generate(n):
+        def generate(n, elems):
             if n == 0: return [""]
             if n == 1: return ["0","1", "8"]
             res = []
-            sequences = generate(n-2)
+            sequences = generate(n-2, ["0","1","8"])
             for sequence in sequences:
-                for i in ["1","8","0"]:
+                for i in elems:
                     res.append(i+sequence+i)
                 res.append("6"+sequence+"9")
                 res.append("9"+sequence+"6")
             return res
-        res = generate(n)
-        if n != 1:
-            for index, sequence in enumerate(res):
-                if sequence[0] == "0":
-                    del(res[index])
-        return res
-        
+        return generate(n, ["1","8"])
--- leetcode/google/399/test_399.py
+++ leetcode/google/399/test_399.py
@@ -8,15 +8,19 @@ class testSolution( unittest.TestCase ):
         self.s = l399.Solution()
     
     def test_solution(self):
+        #import pdb;pdb.set_trace()
         got = self.s.findStrobogrammatic(1)
+        self.assertItemsEqual(got, ['0', '1', '8'])
         print got
         got = self.s.findStrobogrammatic(2)
         print got
         got = self.s.findStrobogrammatic(2)
         self.assertItemsEqual(got, ["11","69","88","96"])
         got = self.s.findStrobogrammatic(3)
+        self.assertItemsEqual(got, ['101', '808', '609', '906', '111', '818', '619', '916', '181', '888', '689', '986'])
         print got
         got = self.s.findStrobogrammatic(4)
+        self.assertItemsEqual(got, ['1111', '8118', '6119', '9116', '1881', '8888', '6889', '9886', '1001', '8008', '6009', '9006', '1691', '8698', '6699', '9696', '1961', '8968', '6969', '9966'])
         print got
 
 
