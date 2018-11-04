1940f35da321a6d7dc6810edee4f9edbc98d8d5c
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-11-04 17:35:30 +0800

    447 RE, wrote heap solution, look good but need to fix some implementation bug

--- leetcode/google/447/l447.py
+++ leetcode/google/447/l447.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Solution(object):
     def kSmallestPairs(self, nums1, nums2, k):
         """
@@ -6,3 +8,17 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        heap = [(num + nums2[0], index, 0)
+                for index, num in enumerate(nums1)]
+        heapq.heapify(heap)
+
+        res = []
+        for _ in xrange(min(k, len(nums1)*len(nums2))):
+            _, index_nums1, index_nums2 = heapq.heappop(heap)
+            res.append([nums1[index_nums1], nums2[index_nums2]])
+            if index_nums2 + 1 < len(nums2):
+                new_node = (nums1[index_nums1] + nums2[index_nums2 + 1],
+                            index_nums1, index_nums2 + 1)
+                heapq.heappush(heap, new_node)
+
+        return res
--- leetcode/google/447/test_447.py
+++ leetcode/google/447/test_447.py
@@ -14,8 +14,20 @@ class testSolution(unittest.TestCase):
         
         got = self.s.kSmallestPairs(nums1, nums2, k)
         self.assertEqual(got, [[1,2],[1,4],[1,6]] )
-
         
+        nums1 = [1,1,2]
+        nums2 = [1,2,3]
+        k = 2
+        got = self.s.kSmallestPairs(nums1, nums2, k)
+        self.assertEqual(got, [[1,1],[1,1]])
+
+        nums1 = [1,2]
+        nums2 = [3]
+        k = 3
+        got = self.s.kSmallestPairs(nums1, nums2, k)
+        self.assertEqual(got, [[1,3],[2,3]])
+
+
 if __name__ == "__main__":
     unittest.main()
 
