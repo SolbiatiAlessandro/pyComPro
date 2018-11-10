f4a2a6154058901c5d82d8211fc89587d1c743b2
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-11-10 18:48:23 +0800

    AC, fastest with hashtable optimization, beats 50% solution

--- leetcode/google/453/l453.py
+++ leetcode/google/453/l453.py
@@ -9,13 +9,21 @@ class Solution(object):
         """
         #import pdb;pdb.set_trace()
         res, nums_with_index = [], sorted(zip(nums, range(len(nums))))
+        answers = dict()
         for query in findNums:
             index = bisect.bisect(nums_with_index, (query, -1))
             original_index = nums_with_index[index][1]
             found = False
             for num in nums[original_index:]:
+                num_answer = answers.get(num)
+                if num_answer > query:
+                    res.append(num_answer)
+                    answers[query] = num_answer
+                    found = True
+                    break
                 if num > query:
                     res.append(num)
+                    answers[query] = num
                     found = True
                     break
             if not found: res.append(-1)
