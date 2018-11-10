99518afcd1929acea61d41af5610205071aadd8b
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-11-10 21:32:44 +0800

    456 WA, without bfs is efficient, but get WA on edge cases, need to rethink

--- leetcode/google/456/l456.py
+++ leetcode/google/456/l456.py
@@ -19,24 +19,17 @@ class Solution(object):
         total = order._qsize()
         for _ in xrange(total):
             first, second = False, False
-            visited = defaultdict(int)
-            start = order.get()[1] 
-            queue = [start]
-            while queue and not (first and second):
-                i, j = queue.pop()
-                height = matrix[i][j]
-                visited[(i, j)] = 1
-                for x, y in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
-                    if x < 0 or y < 0: 
-                        first = True
-                    elif x == len(matrix) or y == len(matrix[0]):
-                        second = True
-                    elif not visited[(x, y)] and matrix[x][y] <= height:
-                        first, second = max(first_res[(x, y)], first),\
-                                        max(second_res[(x, y)], second)
-                        queue.insert(0, (x, y))
-            first_res[start], second_res[start] = first, second
+            height, (i, j) = order.get() 
+            for x, y in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
+                if x < 0 or y < 0: 
+                    first = True
+                elif x == len(matrix) or y == len(matrix[0]):
+                    second = True
+                elif matrix[x][y] <= height:
+                    first, second = max(first, first_res[(x, y)]), max(second_res[(x, y)], second)
+
+            first_res[(i, j)], second_res[(i, j)] = first, second
             if first and second:
-                res_list.append(list(start))
+                res_list.append([i, j])
 
         return res_list
