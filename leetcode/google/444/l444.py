7d4ceb0fb1b6f4d4261d1040bffddf7009230df8
SolbiatiAlessandro <alexsolbiati@hotmail.it>
2018-11-20 00:52:11 +0800

    444 AC, wrote iterative solution in a more compact way

--- leetcode/google/444/l444.py
+++ leetcode/google/444/l444.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         def ok(x, y): return 0 <= x < len(grid) and 0 <= y < len(grid[0])
         def grid_get(x, y): return grid[x][y] if ok(x, y) else 1e9
-        for diagonal in reversed(xrange((len(grid) - 1) + len(grid[0]) - 1)):
+        for xy in reversed(xrange((len(grid) - 1) + len(grid[0]) - 1)):
             for y in xrange(len(grid[0])):
-                x = diagonal - y
-                if ok(x, y): grid[x][y] += min(grid_get(x + 1, y), grid_get(x, y + 1))
+                if ok(xy - y, y):
+                    grid[xy - y][y] += min(grid_get(xy - y + 1, y), grid_get(xy - y, y + 1))
         return grid[0][0]
