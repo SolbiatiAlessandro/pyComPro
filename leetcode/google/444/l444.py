a1596ae19e28ca82fea5ad0fe1b626882b11c316
SolbiatiAlessandro <alexsolbiati@hotmail.it>
2018-11-20 00:10:27 +0800

    444 AC, a bit slow might improve memory in the next version (before was 'bugged' cause I misread the problem statment TAMA)

--- leetcode/google/444/l444.py
+++ leetcode/google/444/l444.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        prev = {}
+        answers = {}
         def solve(x, y):
-            if x == len(grid) or (grid and y == len(grid[0])): return 1e9
             if x == len(grid) - 1 and y == len(grid[0]) - 1: return grid[x][y]
-            prev[(x, y)] = min(solve(x + 1, y), solve(x + 1, y + 1), solve(x, y + 1)) 
-            return min(solve(x + 1, y), solve(x + 1, y + 1), solve(x, y + 1)) + grid[x][y]
-        import pdb;pdb.set_trace()
-        res = solve(0, 0)
-        return res
+            if x == len(grid) or y == len(grid[0]): return 1e9
+            if answers.get((x, y)) is None:
+                answers[(x, y)] = min(solve(x + 1, y), solve(x, y + 1)) + grid[x][y]
+            return answers[(x, y)]
+        return solve(0, 0)
