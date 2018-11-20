06f4ba0321e2cf6bb73514dac2866b674ae6fceb
SolbiatiAlessandro <alexsolbiati@hotmail.it>
2018-11-20 18:45:40 +0800

    466 added a DP structure but still TLE< the subproblem is not optimal

--- leetcode/google/466/l466b.py
+++ leetcode/google/466/l466b.py
@@ -4,10 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        answers = {}
         def solve(visited, pos):
             if pos == len(nums): return 0
-            if not (visited & 1 << (pos + 1)%len(nums)) and not (visited & 1 << (pos - 1)):
-                return max(solve(visited|1<<(pos), pos + 1) + nums[pos], solve(visited, pos + 1))
-            return solve(visited, pos + 1)
-        #import pdb;pdb.set_trace()
+            if answers.get((visited, pos)) is None:
+                if not (visited & 1 << (pos + 1)%len(nums)) and not (visited & 1 << (pos - 1)):
+                    answers[(visited, pos)] = max(solve(visited|1<<(pos), pos + 1) + nums[pos], solve(visited, pos + 1))
+                else: answers[(visited, pos)] = solve(visited, pos + 1)
+            return answers[(visited, pos)]
         return max(solve(0, 1), solve(1, 1) + nums[0]) if nums else 0
