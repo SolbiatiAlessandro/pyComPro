b67c458cc459420e177cc8510da10538efc63ad3
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-11-22 22:16:57 +0800

    479, AC solutions handle multiple vectors beats i90%

--- leetcode/google/479/l479.py
+++ leetcode/google/479/l479.py
@@ -7,35 +7,30 @@ class ZigzagIterator(object):
         :type v2: List[int]
         """
         self.vectors = [v1, v2]
-        self.total = sum(len(v) for v in self.vectors)
-        self.curr = 0
-        self.vector_index, self.char_index = 0, 0
+        self.vector_indexes = []
+        if v1: self.vector_indexes.insert(0, 0)
+        if v2: self.vector_indexes.append(1)
+        self.elem_indexes = [0, 0]
+        self.index = 1
 
     def next(self):
         """
         :rtype: int
         """
-        if not self.hasNext(): return None
-        self.curr += 1
-
-        def advance():
-            self.vector_index = (self.vector_index + 1)
-            if self.vector_index == len(self.vectors):
-                self.vector_index = 0
-                self.char_index += 1
-        
-        while self.char_index >= len(self.vectors[self.vector_index]):
-            advance()
-        res = self.vectors[self.vector_index][self.char_index]
-        advance()
-        return res
+        self.index = (self.index + 1) % len(self.vector_indexes) 
+        index = self.vector_indexes[self.index]   # index of the vector
+        vector = self.vectors[index]              # vector
+        elem = vector[self.elem_indexes[index]]   # element from the vector
+        self.elem_indexes[index] += 1
+        if self.elem_indexes[index] == len(vector):
+            del self.vector_indexes[self.index]   # if vector ended, delete it
+        return elem
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        if self.curr + 1 > self.total: return False
-        return True
+        return bool(self.vector_indexes)
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
