dd9a809c521120e186496f55cab714186fd9ff75
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-11-23 22:20:32 +0800

    477 AC, is really the intended solution BIT? impressed by google..

--- leetcode/google/477/l477.py
+++ leetcode/google/477/l477.py
@@ -4,6 +4,7 @@ class NumMatrix(object):
         """
         :type matrix: List[List[int]]
         """
+        if not matrix: return
         self.original_matrix = [[x for x in row] for row in matrix]
         for i in xrange(1, len(matrix)): matrix[i][0] += matrix[i - 1][0]
         for i in xrange(1, len(matrix[0])): matrix[0][i] += matrix[0][i - 1]
@@ -11,7 +12,8 @@ class NumMatrix(object):
             for j in xrange(1, len(matrix[0])):
                 matrix[i][j] += matrix[i-1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1]
         self.matrix = matrix
-        self.updates = [[] for _ in xrange(len(matrix))]
+        self.updates={}
+      
         
     def update(self, row, col, val):
         """
@@ -20,7 +22,8 @@ class NumMatrix(object):
         :type val: int
         :rtype: void
         """
-        self.updates[row].append((col, val - self.original_matrix[row][col]))
+        self.updates[(row, col)] = val - self.original_matrix[row][col]
+       
         
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -33,7 +36,10 @@ class NumMatrix(object):
         row1 -= 1
         col1 -= 1
         delta = 0
-        for row in xrange(row1 + 1, row2 + 1):
-            for update in self.updates[row]:
-                if col1 < update[0] <= col2: delta += update[1]
-        return self.matrix[row2][col2] - self.matrix[row1][col2] - self.matrix[row2][col1] + self.matrix[row1][col1] + delta
+        for (row, col), val in self.updates.items():
+            if row1 < row <= row2 and col1 < col <= col2: delta += val
+        res = delta + self.matrix[row2][col2]  
+        if row1 >= 0: res -= self.matrix[row1][col2] 
+        if col1 >= 0: res -= self.matrix[row2][col1] 
+        if row1 >= 0 and col1 >= 0: res += self.matrix[row1][col1]
+        return res
