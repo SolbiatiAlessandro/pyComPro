272e1e9ce0354df1f62d7716413019e041fdb9ec
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-12-01 15:08:37 +0800

    (medium) AC beats 80%, realized I was just traversing the graph in the wrong way (DFS instead of BFS)

--- leetcode/mock-interviews/word_ladder/word_ladderB.py
+++ leetcode/mock-interviews/word_ladder/word_ladderB.py
@@ -17,17 +17,19 @@ class Solution(object):
             for i, _ in enumerate(word):
                 groups[(word[:i],word[i+1:])].append(word)
 
-        def compute_distance(word, step):
+        if endWord not in wordList: return 0
+
+        stack = [(endWord, 1)]
+        while stack:
+            word, step = stack.pop()
             for i, _ in enumerate(word):
                 key = (word[:i],word[i+1:])
                 if step < distance[key]:
                     distance[key] = min(distance[key], step)
-
                     for next_word in groups[(word[:i],word[i+1:])]:
                         if next_word != word:
-                            compute_distance(next_word, step + 1)
+                            stack.insert(0, (next_word, step + 1))
 
-        if endWord in wordList: compute_distance(endWord, 1)
         res = 1e9
         for i, _ in enumerate(beginWord):
             res = min(res, distance[(beginWord[:i],beginWord[i+1:])])
