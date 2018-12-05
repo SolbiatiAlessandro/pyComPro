5906858470f0b99fd5be5ae965918603955c2edb
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-12-05 12:59:02 +0800

    problem A, two lines AC

--- codeforces/contest/1088/A.py
+++ codeforces/contest/1088/A.py
@@ -1,13 +1,2 @@
 x = int(raw_input())
-def solve(x):
-    for b in xrange(1, x + 1):
-        for a in xrange(1, x + 1):
-            ok = True
-            if not (a % b == 0): ok =  False
-            if not (a * b > x): ok = False
-            if not (a / b < x): ok = False
-            if ok:
-                return a, b
-    return -1
-got = solve(x)
-print got if type(got) is int else "{} {}".format(got[0], got[1])
+print "{} {}".format(x, x) if x != 1 else -1
