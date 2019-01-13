0593ee28c39b5cab03b634038f98c542a13cddcc
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-01-13 17:37:44 +0100

    909C tried to speed up but still gets TLE

--- codeforces/vplanet/week1/C.py
+++ codeforces/vplanet/week1/C.py
@@ -8,16 +8,29 @@ joint = lambda sep = " ", *args: sep.join(str(i) if type(i) != list else sep.joi
 def iters(): return xrange(int(raw_input()))
 
 def solve(inp):
-    memo = {}
+    memo = {} # memo table for the main dp recursion
+    memo_sum = {} # memo table for the suffix sum used for speed up
     MAXPOS = len(inp) + 1
+
+    def suffix_sum(i, pos):
+        # res = sum([dp(i, j) for j in xrange(pos, MAXPOS)])
+        if memo_sum.get(i):
+            i_sum = memo_sum[i]
+        else:
+            i_sum = [dp(i, j) for j in xrange(MAXPOS)]
+            for i in reversed(xrange(len(i_sum) - 1)): i_sum[i] += i_sum[i + 1]
+            memo_sum[i] = i_sum
+        return i_sum[pos]
+
     def dp(i, pos):
         try:
             if memo.get((i, pos)): return memo[(i, pos)]
             res = -1
-            if (i == 0 and pos > 0) or pos < 0: res = 0
+            if  (i == 0 and pos > 0) or pos < 0: res = 0
             elif i == 0 and pos == 0: res = 1
             elif inp[i - 1] == 'f': res = dp(i - 1, pos - 1)
-            elif inp[i - 1] == 's': res = sum([dp(i - 1, j) for j in xrange(pos, MAXPOS)])
+            elif inp[i - 1] == 's':
+                res = suffix_sum(i - 1, pos)
             memo[(i, pos)] = res
             return res
         except IndexError as e:
