2edd4f772e904604bd93e6130a5b4d707461ef9b
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-03-02 17:52:22 +0100

    1118, now is passing TLE using sys.stdin.readlines but gets RUNTIMEERROR

--- codeforces/contest/1118/F1_upsolving.py
+++ codeforces/contest/1118/F1_upsolving.py
@@ -1,11 +1,12 @@
 """
 this is a standard python template for codeforces task, repo: github.com/solbiatialessandro/pyComPro/codeforces
 """
+import sys
 from sys import stdin as _stdin
 from sys import stdout
 stdin = lambda type_ = "int", sep = " ": list(map(eval(type_), _stdin.readline().split(sep)))
 joint = lambda sep = " ", *args: sep.join(str(i) if type(i) != list else sep.join(map(str, i)) for i in args)
-def iters(): return xrange(int(raw_input()))
+def iters(): return range(int(raw_input()))
 
 def solve(colors, edge_list):
     br = []
@@ -23,7 +24,7 @@ def solve(colors, edge_list):
     traverse(0, -1)
 
     res = 0
-    for node in xrange(1, len(colors)):
+    for node in range(1, len(colors)):
         first_subtree = (br[0][1] - br[node][1], br[0][2] - br[node][2])
         second_subtree = (br[node][1], br[node][2])
         if not(0 not in first_subtree or 0 not in second_subtree):
@@ -34,11 +35,12 @@ def solve(colors, edge_list):
 
 if __name__ == "__main__":
     """the solve(*args) structure is needed for testing purporses"""
-    n = int(raw_input())
-    colors = stdin()
-    edge_list = [[] for _ in xrange(n)]
-    for _ in xrange(n - 1):
-        a, b = stdin()
+    n = int(input())
+    colors = [int(t) for t in input().split(' ')]
+    edge_list = [[] for _ in range(n)]
+    lines = sys.stdin.readlines()
+    for i in range(n - 1):
+        a, b = [int(t) for t in lines[i].split(' ')]
         edge_list[a-1].append(b-1)
         edge_list[b-1].append(a-1)
-    print solve(colors, edge_list)
+    print(solve(colors, edge_list))
